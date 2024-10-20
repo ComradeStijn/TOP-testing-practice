d9a8efa97d14154fe25c10b9ac508269ea5e09ee
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-20 22:59:39 +0200

    feat: Add calculator and tests

--- code.js
+++ code.js
@@ -12,3 +12,18 @@ export function reverse(input) {
   }
   return input;
 }
+
+export const calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  }
+}
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverse } from "./code";
+import { capitalize, reverse, calculator } from "./code";
 import { test, expect } from "@jest/globals";
 
 test("capitalize", () => {
@@ -13,4 +13,11 @@ test("Reverse string", () => {
   expect(reverse("Hallo iedereen")).toMatch("neeredei ollaH");
   expect(reverse("")).toMatch("");
   expect(reverse(23)).toBe(23);
+});
+
+test("Calculator", () => {
+  expect(calculator.add(3, 4)).toBe(7);
+  expect(calculator.subtract(3, 2)).toBe(1);
+  expect(calculator.divide(8, 4)).toBeCloseTo(2);
+  expect(calculator.multiply(4, 5)).toBe(20);
 });
